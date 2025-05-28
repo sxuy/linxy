c4bc4021e3c69c34d44eee957c0b6b3dcbbe7fcc
sunxuyi <sxy200493@icloud.com>
2025-05-29 00:48:13 +0800

    Initial Commit

---
+++ linxy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  linxy
+//
+//  Created by xuyi sun on 5/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ linxy/linxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  linxyApp.swift
+//  linxy
+//
+//  Created by xuyi sun on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct linxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
