eb39102a1696cd01506dfeb5b3763f36a9f7fc04
sunxuyi <sxy200493@icloud.com>
2025-06-08 15:39:09 +0800

    2.8 SwiftUI 下构建一个最简单的视图

--- linxy/ContentView.swift
+++ linxy/ContentView.swift
@@ -1,21 +1,10 @@
-//
-//  ContentView.swift
-//  linxy
-//
-//  Created by xuyi sun on 5/29/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+         VStack {
+            Text("Hi !")
         }
-        .padding()
     }
 }
 
