e6c59d9a3efbc4512f257785e112c38640cd40eb
sunxuyi <sxy200493@icloud.com>
2025-06-09 20:20:19 +0800

    2.10 写一个导航堆栈视图

--- linxy/ContentView.swift
+++ linxy/ContentView.swift
@@ -2,8 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-         VStack {
-            Text("Hi !")
+        NavigationStack {
+            List {
+                Section (header: Text("Dream Record List"), footer: Text("")) {
+                    NavigationLink {Text("Dreamt in peace")} label: {
+                        Label("2025/06/01", systemImage: "moon.stars.fill")
+                    }
+                }
+            }
+            .navigationTitle("Linxy")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Record") {
+                    }
+                } //.primaryAction
+            }
         }
     }
 }
